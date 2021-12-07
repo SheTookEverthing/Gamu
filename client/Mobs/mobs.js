cabf329a9da8bfe5a33a1adfd8dbbb733df45d51
SheTookEverthing <hchen63@buffalo.edu>
2021-12-07 03:19:08 -0500

    figuring out a save file

---
+++ client/Mobs/mobs.js
@@ -0,0 +1,22 @@
+var monsters = {
+    Goblin:{
+        //level & exp
+        level = 0,
+        exp = 0,
+        //spieces
+        speices =  "???",
+        //game stats
+        Heatlh = 10,
+        Mana = 10,
+        Strength = 5,
+        Defense = 5,
+        Dexterity = 5,
+        //res
+        phy_res = 5,
+        mag_res = 5,
+        fire_res = -5,
+        water_res = -5,
+        earth_res = -5,
+        wind_res = -5,
+    }
+}
---
+++ client/Player/characterpage.js
@@ -0,0 +1,34 @@
+var player = {
+    //level & exp
+    level: 0,
+    level_cap: 0,
+    exp: 0,
+    exp_cap: 10,
+    //spieces//
+    speices:  "???",
+    organic: 0,
+    mechanic:  0,
+    magic: 0,
+    //game stats//
+    Heatlh: 10,
+    Mana: 10,
+    Strength: 5,
+    Defense: 5,
+    Dexterity: 5,
+    //bonus//
+    phy_bonus: 0,
+    fire_bonus: 0,
+    water_bonus: 0,
+    earth_bonus: 0,
+    wind_bonus: 0,
+    //res
+    phy_res: 10,
+    mag_res: 10,
+    fire_res: 5,
+    water_res: 5,
+    earth_res: 5,
+    wind_res: 5,
+    //progress
+    tutorial: false,
+    backstory: false,
+}
