696937590988c389052d07c054e18d45eb51732f
SheTookEverthing <hchen63@buffalo.edu>
2021-12-17 02:03:05 -0500

    hope it's the right files

--- client/GameFile/Player/characterpage.js
+++ client/GameFile/Player/characterpage.js
@@ -1,7 +1,7 @@
 var player = {
     //level & exp
     level: 0,
-    level_cap: 0,
+    level_cap: 20,
     exp: 0,
     exp_cap: 10,
     //spieces//
@@ -10,20 +10,19 @@ var player = {
     mechanic:  0,
     magic: 0,
     //game stats//
-    Heatlh: 10,
+    Health: 10,
     Mana: 10,
     Strength: 5,
     Defense: 5,
     Dexterity: 5,
     //bonus//
-    phy_bonus: 0,
-    fire_bonus: 0,
-    water_bonus: 0,
-    earth_bonus: 0,
-    wind_bonus: 0,
+    phy_dmg: 5,
+    fire_dmg: 5,
+    water_dmg: 5,
+    earth_dmg: 5,
+    wind_dmg: 5,
     //res
     phy_res: 10,
-    mag_res: 10,
     fire_res: 5,
     water_res: 5,
     earth_res: 5,
@@ -31,4 +30,4 @@ var player = {
     //progress
     tutorial: false,
     backstory: false,
-}
+};
